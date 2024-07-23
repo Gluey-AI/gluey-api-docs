410c7eea6742b51b8790660b2913924202b0e9e6
Torbjörn Maaherra <tmaaherra@hotmail.com>
2024-07-23 08:46:20 +0100

    fix(pudo): making carrier_id mandatory and adding page number

--- app/api/v1/pudo/endpoints.py
+++ app/api/v1/pudo/endpoints.py
@@ -8,5 +8,5 @@ from app.api.v1.pudo.http_responses.payloads import http_get_pudo_response
 router = APIRouter()
 
 @router.get("/pudo", description="Endpoint to fetch pudo points for a specific carrier / country.", summary="PUDO Points", responses=http_get_pudo_response)
-async def pudo(headers: dict = Depends(common_headers), carrier_id: str = Query('', description="Glueys ID that identifies the carrier in our system, e.g. 'poste_italiane', 'yodel'. The Gluey ID of the carrier as found in the library of carriers in Gluey."), iso_country: str = Query('', description="The ISO Alpha-2 ('GB') or ISO Alpha-3 ('GBR') country code.")) -> list[PudoPoint]:
+async def pudo(headers: dict = Depends(common_headers), carrier_id: str = Query(..., description="Glueys ID that identifies the carrier in our system, e.g. 'poste_italiane', 'yodel'. The Gluey ID of the carrier as found in the library of carriers in Gluey."), iso_country: str = Query('', description="The ISO Alpha-2 ('GB') or ISO Alpha-3 ('GBR') country code."), page: int = Query(1, description="The page number related to the pudo point retrieval")) -> list[PudoPoint]:
     return
