0985497f1c472783522d5fd91cf592e1e5b10465
Torbjörn Maaherra <tmaaherra@hotmail.com>
2024-07-24 13:40:48 +0100

    refactor(label): splitting out print to print + print and book

--- app/api/v1/label/endpoints.py
+++ app/api/v1/label/endpoints.py
@@ -4,21 +4,21 @@ from app.api.v1.common.headers import common_headers
 from app.api.v1.common.models.base_models import Document
 from app.api.v1.label.models.api.base_shipment_response import BaseShipmentResponseModel
 from app.api.v1.label.models.api.carrier import CarrierService
-from app.api.v1.label.models.api.collection import BookingRequest, BookingResponse, CarrierServiceCollectionDates, CarrierServiceDeliveryDates, CarrierServiceTransitTime, CollectionRequest, DeliveryRequest, ServiceAvailabilityRequest, TransitTimeRequest
+from app.api.v1.label.models.api.collection import CarrierServiceCollectionDates, CarrierServiceDeliveryDates, CarrierServiceTransitTime, CollectionRequest, DeliveryRequest, ServiceAvailabilityRequest, TransitTimeRequest
 from app.api.v1.label.models.api.create_shipment import CreateShipmentRequest
-from app.api.v1.label.models.api.print_label import PrintLabelRequest, PrintLabelSyncResponse
+from app.api.v1.label.models.api.print_label import CollectionBookingResponse, PrintAndBookRequest, PrintAndBookSyncResponse, PrintLabelRequest, PrintLabelSyncResponse, UpdateCollectionRequest
 from app.api.v1.label.models.api.print_documents import PrintDocumentsRequest
 from app.api.v1.label.models.api.shipment import Shipment
 
 from app.api.v1.label.http_responses.payloads import http_create_shipment_response, http_get_shipment_response, http_delete_shipment_response, http_get_labels_response, http_get_documents_response
 from app.api.v1.label.http_responses.request_examples import collection_request_examples, service_availability_request_examples, delivery_request_examples
-from app.api.v1.label.http_responses.response_examples import http_responses_cancel, http_responses_collection_times, http_responses_service_availability, http_responses_booking, http_responses_delivery_times
+from app.api.v1.label.http_responses.response_examples import http_responses_cancel, http_responses_collection_times, http_responses_service_availability, http_responses_delivery_times
 from app.api.v1.label.models.api.update_shipment import UpdateShipmentRequest
 
 
 router = APIRouter()
 
-@router.post("/shipments", description="Endpoint to create a shipment in Gluey.", summary="Create Shipment", responses=http_create_shipment_response, status_code=status.HTTP_201_CREATED)
+@router.post("/shipments", description="Endpoint to create a shipment synchronous in Gluey.", summary="Create Shipment Sync", responses=http_create_shipment_response, status_code=status.HTTP_201_CREATED)
 async def create_shipment(payload: CreateShipmentRequest, headers: dict = Depends(common_headers)) -> BaseShipmentResponseModel:
     return
 
@@ -26,13 +26,13 @@ async def create_shipment(payload: CreateShipmentRequest, headers: dict = Depend
 async def get_shipment(id: str = Path(..., description="Glueys own unique identifier of the shipment"), headers: dict = Depends(common_headers)) -> Shipment:
     return
 
-@router.put("/shipments/{id}", description="Endpoint to update a previously created shipment.", summary="Update Shipment", status_code=status.HTTP_200_OK)
-async def update_shipment(payload: UpdateShipmentRequest, id: str = Path(..., description="Glueys own unique identifier of the shipment"), headers: dict = Depends(common_headers)):
-    return
+# @router.put("/shipments/{id}", description="Endpoint to update a previously created shipment.", summary="Update Shipment", status_code=status.HTTP_200_OK)
+# async def update_shipment(payload: UpdateShipmentRequest, id: str = Path(..., description="Glueys own unique identifier of the shipment"), headers: dict = Depends(common_headers)):
+#     return
 
-@router.delete("/shipments/{id}", description="Endpoint to cancel / delete a previously created shipment.", summary="Cancel Shipment", responses=http_delete_shipment_response, status_code=status.HTTP_204_NO_CONTENT)
-async def delete_shipment(id: str = Path(..., description="Glueys own unique identifier of the shipment"), headers: dict = Depends(common_headers)):
-    return
+# @router.delete("/shipments/{id}", description="Endpoint to cancel / delete a previously created shipment.", summary="Cancel Shipment", responses=http_delete_shipment_response, status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_shipment(id: str = Path(..., description="Glueys own unique identifier of the shipment"), headers: dict = Depends(common_headers)):
+#     return
 
 
 @router.post("/shipments/{id}/carrier/services", description="Endpoint to check what carrier services are available in Gluey for a specific shipment.", summary="Check Service Availability", tags=['Service Availability'], responses=http_responses_service_availability, status_code=status.HTTP_200_OK)
@@ -63,26 +63,33 @@ async def request_transit_times(
     headers: dict = Depends(common_headers)) -> list[CarrierServiceTransitTime]:
     return
 
-@router.post("/shipments/{id}/documents", description="Endpoint to print all documents related to a shipment", summary="Documents", tags=['Print'], responses=http_get_documents_response)
+@router.post("/shipments/{id}/labels", description="Endpoint to print labels synchronous for all parcels in a shipment, and optionally, book a collection date and / or delivery date.", summary="Print Labels and Book Collection Sync", tags=['Print & Book'],responses=http_get_labels_response, status_code=status.HTTP_201_CREATED)
+async def print_labels_book_sync(request: PrintAndBookRequest, id: str = Path(..., description="Glueys own unique identifier of the shipment"), headers: dict = Depends(common_headers)) -> PrintAndBookSyncResponse:
+    return
+
+@router.post("/shipments/{id}/documents", description="Endpoint to print all documents related to a shipment", summary="Print Documents", tags=['Print & Book'], responses=http_get_documents_response)
 async def generate_documents(payload: PrintDocumentsRequest, id: str = Path(..., description="Glueys own unique identifier of the shipment"), headers: dict = Depends(common_headers)) -> list[Document]:
     return
 
-@router.post("/shipments/{id}/labels", description="Endpoint to print labels synchronous, and book a collection, for all parcels in a shipment.", summary="Labels Sync", tags=['Print'],responses=http_get_labels_response)
-async def print_labels_sync(request: PrintLabelRequest, id: str = Path(..., description="Glueys own unique identifier of the shipment"), headers: dict = Depends(common_headers)) -> PrintLabelSyncResponse:
-    return
-
-@router.post("/shipments/{id}/labels/async", description="Endpoint to have Gluey process labels, and book a collection, in the background. Printed labels will be pushed via webhook subscribers when available.", summary="Labels Async", tags=['Print'], status_code=status.HTTP_202_ACCEPTED)
+@router.post("/shipments/{id}/labels/async", description="Endpoint to have Gluey print labels in the background. Printed labels will be pushed via webhook subscribers when available.", summary="Print Labels Async", tags=['Print & Book'], status_code=status.HTTP_202_ACCEPTED)
 async def print_labels_async(request: PrintLabelRequest, id: str = Path(..., description="Glueys own unique identifier of the shipment"), headers: dict = Depends(common_headers)):
     return
 
-@router.post("/shipments/{id}/collection", description="Endpoint to book a collection for a shipment. A pre-requisite is that it has been printed.", summary="Book", tags=['Collect'], responses=http_responses_booking, status_code=status.HTTP_201_CREATED)
+# @router.post("/shipments/{id}/collection", description="Collection services only. Endpoint to print labels and book a collection date for a shipment.", summary="Print Labels and Book Collection Sync", tags=['Collection'], status_code=status.HTTP_201_CREATED)
+# async def book_collection(
+#     payload: PrintAndBookRequest,
+#     id: str = Path(..., description="Glueys own unique identifier of the shipment"),
+#     headers: dict = Depends(common_headers)) -> PrintAndBookSyncResponse:
+#     return
+
+@router.put("/shipments/{id}/collection", description="Endpoint to reschedule the collection of a shipment.", summary="Reschedule Collection", tags=['Manage Bookings'], status_code=status.HTTP_201_CREATED)
 async def book_collection(
-    payload: BookingRequest,
+    payload: UpdateCollectionRequest,
     id: str = Path(..., description="Glueys own unique identifier of the shipment"),
-    headers: dict = Depends(common_headers)) -> BookingResponse:
+    headers: dict = Depends(common_headers)) -> CollectionBookingResponse:
     return
 
-@router.delete("/shipments/{id}/collection", description="Endpoint to cancel a collection that is booked with the carrier.", summary="Cancel", tags=['Collect'], responses=http_responses_cancel, status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/shipments/{id}/collection", description="Endpoint to cancel a collection that is booked with the carrier.", summary="Cancel Collection", tags=['Manage Bookings'], responses=http_responses_cancel, status_code=status.HTTP_204_NO_CONTENT)
 async def cancel_collection(
     id: str = Path(..., description="Glueys own unique identifier of the shipment"),
     headers: dict = Depends(common_headers)):
